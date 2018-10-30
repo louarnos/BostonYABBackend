b983bb5c13f6d78f7354849646766061bc58c2bc
louarnos <louarnos@gmail.com>
2018-10-29 21:45:46 -0400

    Add endpoint to create post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,6 +1,35 @@
 const Post = require('../models/post.model');
 
 //Simple version, without validation or sanitation
-exports.test = function (req, res) {
+const test = (req, res ) =>  {
     res.send('Greetings Evie Bruh!');
+};
+
+const add = ( req, res, next ) => {
+    let body   = req.body.body
+    let title  = req.body.title
+    let author = req.body.author
+    if ( body && title && author ) {
+        let post = new Post({
+            body: body,
+            title: title,
+            author: author
+        })
+
+        post.save()
+            .then( post => {
+                res.json({ post })
+            })
+            .catch( err => {
+                res.json( { err } )
+            })
+    } else {
+        res.json( { message: "You must provide a body, title, and author to create a post" } )
+    }
+}
+
+
+module.exports = {
+    test: test,
+    add: add,
 };
--- routes/post.route.js
+++ routes/post.route.js
@@ -6,5 +6,6 @@ const postController = require('../controllers/post.controller');
 
 // a simple test url to check that all of our files are communicating correctly.
 router.get('/test', postController.test);
+router.post('/add', postController.add);
 
 module.exports = router;
