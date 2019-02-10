b464f8ac23adfccc3c069505004ec9ee2a74e6e4
louarnos <louarnos@gmail.com>
2019-02-10 12:40:41 -0500

    Adjust CORS to handle for dev server k

--- middleware/parsing.js
+++ middleware/parsing.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app     = express();
 
+
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 
@@ -10,6 +11,14 @@ const cookieParser = require('cookie-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use( ( req, res, next ) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Content-Type', 'application/json');
+    next();
+});
 
 module.exports = {
     app
