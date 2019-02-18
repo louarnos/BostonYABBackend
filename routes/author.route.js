ce9ce4facb52ec2b3279a8a8ad321ffc68049a2d
louarnos <louarnos@gmail.com>
2019-02-18 17:43:27 -0500

    Author route should handle for image upload

--- routes/author.route.js
+++ routes/author.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router  = express.Router();
+const Upload  = require('../middleware/multer').upload;
 
 const authorController = require('../controllers/author.controller');
 
 // a simple test url to check that all of our files are communicating correctly.
-router.post('/add', authorController.create);
+router.post('/add', Upload.array('file', 1), authorController.create);
 
 module.exports = router;
