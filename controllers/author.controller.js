34fc6f5019b68d29a0edaa6e894cfd5fb4344cd0
louarnos <louarnos@gmail.com>
2019-02-21 20:13:29 -0500

    Build out author creation controller

--- controllers/author.controller.js
+++ controllers/author.controller.js
@@ -1,8 +1,27 @@
 const Author = require('../models/author.model');
 
 const create = (req, res ) => {
-    console.log( req.body );
-    res.json({ message: 'hit it'})
+    let name     = req.body.name
+    let pronouns = req.body.pronouns
+    let filepath =  req.files[0].filename
+
+    if ( name ) {
+        let authorData = { name };
+        if ( pronouns ) { authorData.pronouns       = pronouns }
+        if ( filepath ) { authorData.profilePicture = filepath }
+
+        let author = new Author(authorData)
+
+        author.save()
+            .then( post => {
+                res.json({ author })
+            })
+            .catch( err => {
+                res.json( { err } )
+            })
+    } else {
+        res.json( { message: "You must provide a name to create an author" } )
+    }
 };
 
 module.exports = {
